ed423a32f57cde84084be493890415af08285615
Natalia Fuoco <99039174+NFNat@users.noreply.github.com>
2022-10-25 23:47:51 -0300

    primer commit

---
+++ code.js
@@ -0,0 +1,166 @@
+// Modal Hard Skils
+
+const openModalHard = document.querySelector(".hard-skills"); 
+const modalHard =document.querySelector(".modal__Hard");
+const closeModalHard = document.querySelector(".modal__close")
+
+openModalHard.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalHard.classList.add("modal--show")    
+})
+closeModalHard.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalHard.classList.remove("modal--show")    
+})
+
+
+//Modal Soft Skills
+
+const openModalSoft = document.querySelector(".soft-skills"); 
+const modalSoft =document.querySelector(".modal__Soft");
+const closeModalSoft = document.querySelector(".modal__close__soft")
+
+openModalSoft.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalSoft.classList.add("modal--show")    
+})
+closeModalSoft.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalSoft.classList.remove("modal--show")    
+})
+
+//Modal Contacto
+
+const openModalContacto = document.querySelector("[contact]"); 
+const modalContact = document.querySelector(".modal__Contacto");
+const closeModalContact = document.querySelector(".modal__close__contacto")
+
+openModalContacto.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalContact.classList.add("modal--show")    
+})
+closeModalContact.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalContact.classList.remove("modal--show")    
+})
+
+
+//Modal Contacto2
+
+const openModalContacto2 = document.querySelector("[contact2]"); 
+const modalContact2 = document.querySelector(".modal__Contacto");
+const closeModalContact2 = document.querySelector(".modal__close__contacto")
+
+openModalContacto2.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalContact2.classList.add("modal--show")    
+})
+closeModalContact2.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalContact2.classList.remove("modal--show")    
+})
+
+//Modal proyectos
+
+const openModalProyectos = document.querySelector(".projects"); 
+const modalProject = document.querySelector(".modal__Proyectos");
+const closeModalProject = document.querySelector(".modal__close__proyectos")
+
+openModalProyectos.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalProject.classList.add("modal--show")    
+})
+closeModalProject.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalProject.classList.remove("modal--show")    
+})
+
+//Modal proyectos
+
+const openModalExperiencia = document.querySelector(".experience"); 
+const modalExperiencia = document.querySelector(".modal__Experiencia");
+const closeModalExperiencia = document.querySelector(".modal__close__experiencia")
+
+openModalExperiencia.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalExperiencia.classList.add("modal--show")    
+})
+closeModalExperiencia.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalExperiencia.classList.remove("modal--show")    
+})
+
+//Modal Hobbies
+
+const openModalHobbies= document.querySelector(".hobbies"); 
+const modalHobbies= document.querySelector(".modal__Hobbies");
+const closeModalHobbies = document.querySelector(".modal__close__hobbies")
+
+openModalHobbies.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalHobbies.classList.add("modal--show")    
+})
+closeModalHobbies.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalHobbies.classList.remove("modal--show")    
+})
+
+//Modal Hobbies
+
+const openModalLanguajes = document.querySelector(".languages"); 
+const modalLanguajes= document.querySelector(".modal__Languages");
+const closeModalLanguajes = document.querySelector(".modal__close__languages")
+
+openModalLanguajes.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalLanguajes.classList.add("modal--show")    
+})
+closeModalLanguajes.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalLanguajes.classList.remove("modal--show")    
+})
+
+//Modal Educacion
+
+const openModalEducation = document.querySelector(".education"); 
+const modalEducation= document.querySelector(".modal__Education");
+const closeModalEducation = document.querySelector(".modal__close__education")
+
+openModalEducation.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalEducation.classList.add("modal--show")    
+})
+closeModalEducation.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalEducation.classList.remove("modal--show")    
+})
+
+//Modal Mondrian
+
+const openModalMondrian = document.querySelector(".mondrian"); 
+const modalMondrian= document.querySelector(".modal__Mondrian");
+const closeModalMondrian = document.querySelector(".modal__close__mondrian")
+
+openModalMondrian.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalMondrian.classList.add("modal--show")    
+})
+closeModalMondrian.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalMondrian.classList.remove("modal--show")    
+})
+
+//Modal Mondrian
+
+const openModalCrud = document.querySelector(".crud"); 
+const modalCrud= document.querySelector(".modal__Crud");
+const closeModalCrud = document.querySelector(".modal__close__crud")
+
+openModalCrud.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalCrud.classList.add("modal--show")    
+})
+closeModalCrud.addEventListener('click', ()=>{
+    event.preventDefault();
+    modalCrud.classList.remove("modal--show")    
+})
